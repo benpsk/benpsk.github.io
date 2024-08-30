ba5a36189ad658d546315cc05a1684afa4302e83
Ben Gunn <bengunn.dev@gmail.com>
2024-08-30 11:26:23 +0700

    initial

---
+++ app.js
@@ -0,0 +1,17 @@
+const projects = [
+    {
+        title: "Install PHP",
+        description: "Effortlessly set up and launch your PHP tech stack on UBUNTU with a single command.",
+        url: "https://github.com/benpsk/install-php"
+    },
+    {
+        title: "Laravel Docker",
+        description: "Docker setup for laravel project. It's a bit messy but work perfectly fine like cron and supervisor (queue worker). Check it out, maybe you don't wanna use it. LOL",
+        url: "https://github.com/benpsk/laravel-docker"
+    },
+    {
+        title: "Hello BKK",
+        description: "A Gatsby blog project for myanmar who wanna migrate to thailand. MDX file based project, no backend, no database just md file. I love it.",
+        url: "https://hellobkk.live/mm/"
+    },
+];
