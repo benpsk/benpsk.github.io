addd57f6795586580f0bef79ea9ec2a522ebb888
Ben Gunn <bengunn.dev@gmail.com>
2025-05-20 01:39:46 +0700

    Update url

--- app.js
+++ app.js
@@ -6,23 +6,23 @@ const projects = [
   },
   {
     title: "Laravel Docker",
-    description: "Docker setup for laravel project. It's a bit messy but work perfectly fine like cron and supervisor (queue worker). Check it out, maybe you don't wanna use it. LOL",
+    description: "Docker setup for laravel project. It's a bit messy but works perfectly fine like cron and supervisor (queue worker). Check it out, maybe you don't wanna use it. LOL",
     url: "https://github.com/benpsk/laravel-docker"
   },
   {
     title: "Hello BKK",
-    description: "The landing page for immigrant who want to migrate to Bangkok. Guide blog for visa process, visa extension and the most trusted Bangkok Agencies.",
+    description: "The landing page for immigrants who want to migrate to Bangkok. Guide blog for visa process, visa extension and the most trusted Bangkok Agencies.",
     url: "https://hellobkk.live"
   },
   {
     title: "Blog | Hello BKK",
-    description: "A Gatsby blog project for myanmar who wanna migrate to thailand. MDX file based project, no backend, no database just md file. I love it.",
+    description: "A Gatsby blog project for Myanmar who wanna migrate to Thailand. MDX file-based project, no backend, no database, just md file. I love it.",
     url: "https://blog.hellobkk.live/"
   },
   {
     title: "BKK Agencies",
-    description: "The most trusted agencies for Bangkok immigrants. Build with PHP Laravel, Inertia, React and Postgres. I love working with Laravel Inertia, so productive.",
-    url: "https://agencies.hellobkk.live/"
+    description: "The most trusted agencies for Bangkok immigrants. Built with PHP Laravel, Inertia, React and Postgres. I love working with Laravel Inertia, so productive.",
+    url: "http://agencies.hellobkk.live/"
   },
 ];
 
